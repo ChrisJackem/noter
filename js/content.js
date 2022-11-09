547d4d7dd07d3207ee074ebad9c409cba62da655
Chris Jackem <chrisjackem@gmail.com>
2022-11-08 19:57:13 -0500

    Working Version
    Added settings, made tooltip hideable, some random fixes

--- js/content.js
+++ js/content.js
@@ -1,85 +1,90 @@
-var selected = null
-const arrow_size = 20
 
-//chrome.runtime.connect()
+// Do not run if show_tooltip is false
+chrome.storage.sync.get('show_tooltip', response => {
+    if ( response.hasOwnProperty('show_tooltip') && !response.show_tooltip ) return
 
-// Factories
-const createNode = ({ type, id, classes, html } = obj ) => {  
-    const node = document.createElement(type)
-    if (id){ node.setAttribute('id', id) }
-    if (classes){ classes.forEach( c => node.classList.add(c) )}
-    if (html){ node.innerHTML = html }
-    return node
-}
+    var selected = null
+    const arrow_size = 20
 
-const createBtn = (name, classes, on_click) =>{
-    const btn = createNode({ type: 'button', id: name, html: name,
-        classes: classes ? [...classes, 'noter-btn'] : ['noter-btn', 'rounded']        
-    })   
-    btn.addEventListener( 'click', on_click )
-    return btn
-}
-
-const escapeHTML = str =>{
-    let text = document.createTextNode(str)
-    let p = document.createElement('p')
-    p.appendChild(text)
-    return p.innerHTML
-}
-
-// ToolTip 
-const tool_tip = document.body.appendChild( createNode(
-     { type:'div', id:'tool-tip', classes:['hidden']//, html:'Noter'
-}))
-
-tool_tip.appendChild( createBtn( 'Copy', null, e =>{
-    navigator.clipboard.writeText( selected )
-}))
-
-//chrome.storage.sync.set({notes: []})
-tool_tip.appendChild( createBtn( 'Add', null, e =>{
-    if (selected) {
-
-        // Append new object to note_data
-        chrome.storage.sync.get('notes', note_data => {            
-            const url = window.location.href
-            const date = new Date().toLocaleString()
-            const new_note = {
-                name: date,
-                collapsed: false,
-                url: url,
-                text: escapeHTML(selected)
-                
-            }
-            note_data.notes.push(new_note)
-            console.log('notes', note_data.notes)
-            chrome.storage.sync.set({notes: note_data.notes})
-        })
-
-        tool_tip.classList.add('hidden')
+    // Factories
+    const createNode = ({ type, id, classes, html } = obj ) => {  
+        const node = document.createElement(type)
+        if (id){ node.setAttribute('id', id) }
+        if (classes){ classes.forEach( c => node.classList.add(c) )}
+        if (html){ node.innerHTML = html }
+        return node
     }
-}))
+
+    const createBtn = (name, classes, on_click) =>{
+        const btn = createNode({ type: 'button', id: name, html: name,
+            classes: classes ? [...classes, 'noter-btn'] : ['noter-btn', 'rounded']        
+        })   
+        btn.addEventListener( 'click', on_click )
+        return btn
+    }
+
+    const escapeHTML = str =>{
+        let text = document.createTextNode(str)
+        let p = document.createElement('p')
+        p.appendChild(text)
+        return p.innerHTML
+    }
+
+    // ToolTip 
+    const tool_tip = document.body.appendChild( createNode(
+        { type:'div', id:'tool-tip', classes:['hidden']//, html:'Noter'
+    }))
+
+    tool_tip.appendChild( createBtn( 'Copy', null, e =>{
+        navigator.clipboard.writeText( selected )
+    }))
+
+    //chrome.storage.sync.set({notes: []})
+    tool_tip.appendChild( createBtn( 'Add', null, e =>{
+        if (selected) {
+
+            // Append new object to note_data
+            chrome.storage.sync.get('notes', note_data => {            
+                const url = window.location.href
+                const date = new Date().toLocaleString()
+                const new_note = {
+                    name: date,
+                    collapsed: false,
+                    url: url,
+                    text: escapeHTML(selected)
+                    
+                }
+                if (!note_data.hasOwnProperty('notes')) note_data.notes = []
+                note_data.notes.push(new_note)
+                console.log('notes', note_data.notes)
+                chrome.storage.sync.set({notes: note_data.notes})
+            })
+
+            tool_tip.classList.add('hidden')
+        }
+    }))
 
 
-addEventListener('resize', e => tool_tip.classList.add('hidden') )
+    addEventListener('resize', e => tool_tip.classList.add('hidden') )
 
-document.addEventListener('mouseup', () => {
-    console.log('UP')
-    const selection = document.getSelection();
-    const anchor_node = selection.anchorNode;
-    const focus_node = selection.focusNode;
-    selected = selection.toString()
+    document.addEventListener('mouseup', () => {
+        const selection = document.getSelection();
+        const anchor_node = selection.anchorNode;
+        const focus_node = selection.focusNode;
+        selected = selection.toString()
 
-    // No selection
-    if ( !selection || selection.isCollapsed /* ||  anchor_node != focus_node  */){
-        tool_tip.classList.add('hidden')
-        return
-    }    
-    const selected_text = anchor_node.data.substring(selection.anchorOffset, selection.focusOffset);  
-    const range_rect = selection.getRangeAt(0).getBoundingClientRect();
-    const x = (range_rect.x + ( range_rect.width / 2 ) ) + scrollX
-    const y = range_rect.top + scrollY - arrow_size    
-    tool_tip.style.left = `${x}px`;
-    tool_tip.style.top = `${y}px`;    
-    tool_tip.classList.remove('hidden')
-  })
+        // No selection
+        if ( !selection || selection.isCollapsed /* ||  anchor_node != focus_node  */){
+            tool_tip.classList.add('hidden')
+            return
+        }    
+        const selected_text = anchor_node.data.substring(selection.anchorOffset, selection.focusOffset);  
+        const range_rect = selection.getRangeAt(0).getBoundingClientRect();
+        const x = (range_rect.x + ( range_rect.width / 2 ) ) + scrollX
+        const y = range_rect.top + scrollY - arrow_size    
+        tool_tip.style.left = `${x}px`;
+        tool_tip.style.top = `${y}px`;    
+        tool_tip.classList.remove('hidden')
+    })
+
+})
--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,80 @@
-console.log('Popup js')
-const settings = document.getElementById('settings')
-const content = document.getElementById('content')
-const actions = document.getElementById('actions')
 
-const btn_full = document.getElementById('full-btn')
+const actions = document.getElementById('actions')
+const content = document.getElementById('content')
+const tool_menu = document.getElementById('tools')
+const tool_btns = [...tool_menu.getElementsByClassName('tool-btn')]
+const show_tooltip_checkbox = document.getElementById('tooltip-box')
+const getAllNotes = ()=> [...content.getElementsByClassName('note')]
 
 let note_array = null
 
-//chrome.runtime.connect()
+////////////////////////////////////////////////////////////////// Tools 
+// Tool buttons at the top. This basically acts like a tab menu
+for ( const tool_btn of tool_btns ){
+    tool_btn.onclick = e =>{
+        // Reset everything
+        [...tool_menu.getElementsByClassName('top-menu')].forEach( d => d.classList.add('hidden'))
+        tool_btns.forEach( tb =>{
+            if ( tb === tool_btn ) return
+            tb.classList.add( 'unselected' )
+        })
+        // Toggle btn and div id set in data-div
+        tool_btn.classList.toggle('unselected')
+        if ( !tool_btn.classList.contains('unselected') ){
+            const div_id = tool_btn.getAttribute('data-div')
+            document.getElementById(div_id).classList.remove('hidden')
+        }
+    }
+}
 
-btn_full.onclick = 
-    e => chrome.tabs.create({url:'html/default_popup.html'});
+document.getElementById('full-btn').onclick = e =>
+    chrome.tabs.create({url:'html/default_popup.html'})
+    
+// TODO : We can slim this down by combining with the individual buttons
+const collapse_all = collapse =>{
+    //const all_notes = content.getElementsByClassName('note')
+    for ( const note of getAllNotes() ){
+        const collapse_div = note.getElementsByClassName('note-collapse')[0]
+        const collapse_btn = note.getElementsByClassName('btn-collapse')[0]
+        const is_collapsed = collapse_div.classList.contains('collapsed')        
+        if ( collapse ){
+            if ( !is_collapsed ){
+                collapse_btn.innerHTML = '+'
+                collapse_div.classList.add('collapsed') 
+            }
+        }else{
+            if ( is_collapsed ){
+                collapse_btn.innerHTML = '-'
+                collapse_div.classList.remove('collapsed') 
+            }
+        }
+    }
+}
+document.getElementById('collapse-all').onclick = e => collapse_all( true )
+document.getElementById('uncollapse-all').onclick = e => collapse_all( false )
+
+document.getElementById('delete-all').onclick = e => {
+    note_array = []
+    chrome.storage.sync.set({notes: note_array})
+    getAllNotes().forEach( n => n.remove() )
+
+    /* for ( const note of getAllNotes() ){
+        note.getElementsByClassName('btn-dismiss')[0].onclick()
+    } */
+}
 
 
+show_tooltip_checkbox.onclick = e =>{
+    chrome.storage.sync.set({show_tooltip: e.currentTarget.checked})
+}
+chrome.storage.sync.get('show_tooltip', response => {
+    if (!response.hasOwnProperty('show_tooltip')) response.show_tooltip = true
+    show_tooltip_checkbox.checked = response.show_tooltip
+})
 
 
+//////////////////////////////////////////////////////////////// Notes
 const addNote = ( index, name, url, text, collapsed )=>{
-    console.log(collapsed)
     // Create nodes from all this
     const new_note = content.appendChild( new DOMParser().parseFromString(
         `<div class='note' data-index=${index}>
@@ -24,8 +82,9 @@ const addNote = ( index, name, url, text, collapsed )=>{
                 <h4 class='flex-left'>${name}</h4>
                 <input type='text' class='hidden' value='${name}'>
                 <button class='btn-rename'>Rename</button>
+                <button class='btn-copy'>Copy</button>
                 <button class='btn-collapse'>${collapsed ? '+' : '-'}</button>
-                <button class='btn-dismiss'>delete</button>
+                <button class='btn-dismiss'>x</button>
             </div>
             <div class='note-collapse ${collapsed ? 'collapsed' : ''}'>
                 <div class='note-inner'>
@@ -44,22 +103,12 @@ const addNote = ( index, name, url, text, collapsed )=>{
     const btn_collapse = new_note.getElementsByClassName('btn-collapse')[0]
     const btn_dismiss = new_note.getElementsByClassName('btn-dismiss')[0]
     const btn_rename = new_note.getElementsByClassName('btn-rename')[0]
+    const btn_copy = new_note.getElementsByClassName('btn-copy')[0]
     const note_anchor = new_note.getElementsByClassName('note-link')[0]
 
-    // Activate link
     note_anchor.onclick = 
         e => chrome.tabs.create({ active:true, url:note_anchor.href })
 
-    // Collapse button
-    btn_collapse.onclick = e =>{
-        const collapse_div = new_note.getElementsByClassName('note-collapse')[0]        
-        const is_collapsed = [...collapse_div.classList].includes('collapsed')
-        btn_collapse.innerHTML = is_collapsed ? '-' : "+"    
-        collapse_div.classList.toggle('collapsed')
-        setNoteData(index, 'collapsed', !is_collapsed)
-    }
-
-    // Rename button
     btn_rename.onclick = e =>{
         const header_showing = [...note_title_h.classList].includes('hidden')
         note_title_input.classList.toggle('hidden')
@@ -74,28 +123,39 @@ const addNote = ( index, name, url, text, collapsed )=>{
             note_title_h.innerHTML = note_title_input.value
             setNoteData( index, 'name', note_title_input.value )
         }
-
     }
+
+    btn_collapse.onclick = e =>{
+        const collapse_div = new_note.getElementsByClassName('note-collapse')[0]        
+        const is_collapsed = [...collapse_div.classList].includes('collapsed')
+        btn_collapse.innerHTML = is_collapsed ? '-' : "+"    
+        collapse_div.classList.toggle('collapsed')
+        setNoteData(index, 'collapsed', !is_collapsed)
+    }
+
+    btn_dismiss.onclick = e =>{        
+        note_array.splice( index, 1 )
+        chrome.storage.sync.set({notes: note_array})
+        new_note.remove()
+    }
+
+    btn_copy.onclick = e => navigator.clipboard.writeText( text )
 }
 
 const setNoteData = (index, prop, new_val) =>{
-    console.log(index, prop, new_val)
-    //console.log(note_array)
     const obj = note_array[index]
     console.log(obj)
     obj[prop] = new_val
-    //console.log(note_array)
     chrome.storage.sync.set({notes: note_array})
 }
 
 const getNoteData = (() => {
     chrome.storage.sync.get('notes', note_data => {
         note_array = note_data.notes
-        console.log(note_array)
+        if (!note_array) return
         note_array.forEach( (note, i)=>{
             const { name, text, url, collapsed } = note
             addNote( i, name, url, text, collapsed )
-            //index, name, url, text, collapsed
         })
     });
 })()
